f131648ccac4c8a14bdd34eef470fa48d150bf54
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-01-22 23:05:17 +0530

    Object Modeling and relationships

---
+++ Day4/Sample-Programs/FacultyDetails.java
@@ -0,0 +1,33 @@
+//import java.util.ArrayList;
+////Create faculty class
+//class Faculty {
+//String facultyName;
+//
+//Faculty(String facultyName){
+// this.facultyName = facultyName;
+// }
+// }
+//
+// class Department {
+//
+// String departmentName;
+// ArrayList <Faculty> faculties;
+//
+//  Department(String departmentName){
+// this.departmentName = departmentName;
+// this.faculties = new ArrayList<>();
+// }
+//
+// }
+//class University  {
+//
+// String universityName;
+// ArrayList <Department> departments;
+//
+// Department(String departmentName){
+//  this.universityName = universityName;
+//  this.departments = new ArrayList<>();
+// }
+//
+//}
+//

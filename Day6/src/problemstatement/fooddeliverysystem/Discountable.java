de4914fa0a729ee1552e1b6b7dffc59c48624ec4
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-01-26 22:34:06 +0530

    I am done with my food delivery system problem.

---
+++ Day6/src/problemstatement/fooddeliverysystem/Discountable.java
@@ -0,0 +1,7 @@
+package problemstatement.fooddeliverysystem;
+
+// Interface Discountable
+interface Discountable {
+    void applyDiscount(double discountPercentage);
+    String getDiscountDetails();
+}
